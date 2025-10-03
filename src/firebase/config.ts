285091d03bb2557c7cc03ae807a09b5c9f2892c0
augusto lourenço <augustojunior8@gmail.com>
2025-10-03 02:30:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1387046892-2f973",
+  "appId": "1:156716405737:web:615ebf00201638628df178",
+  "apiKey": "AIzaSyA6vWX2SWFDL_XwyWcstOLWmEtqmGdARhk",
+  "authDomain": "studio-1387046892-2f973.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "156716405737"
+};
