db21e5764acff452349814c9cf1298383839f12d
augusto lourenço <augustojunior8@gmail.com>
2025-10-03 01:59:10 +0000

    o botaão está funcionando, mas ao clicar não vejo o tema  escoilhido sen

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+function ThemeWrapper({ children }: { children: React.ReactNode }) {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    const themes = ["jungle", "ocean", "galaxy", "sunset", "default"];
+    document.body.classList.remove(...themes.map((t) => `theme-${t}`));
+    if (theme && theme !== "default") {
+        document.body.classList.add(`theme-${theme}`);
+    } else {
+        document.body.classList.add('theme-default');
+    }
+  }, [theme]);
+
+  return <>{children}</>;
+}
+
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+      <NextThemesProvider {...props}>
+        <ThemeWrapper>{children}</ThemeWrapper>
+      </NextThemesProvider>
+  );
 }
